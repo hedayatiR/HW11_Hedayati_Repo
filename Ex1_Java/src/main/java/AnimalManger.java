4e74699c8d812d88b0f4914a3f309e9c53a1e196
hedayatiR <gunner_1369@yahoo.com>
2019-06-09 12:06:18 +0430

    Part3: Code More refactoring.

---
+++ Ex1_Java/src/main/java/AnimalManger.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class AnimalManger {
+
+    public List<Animal> createCatList(){
+        List<Animal> animalList = new ArrayList<>();
+        int i = 1;
+        for (CatBreedEnum catBreedEnum : CatBreedEnum.values()) {
+
+            Cat cat = new Cat("cat"+i);
+            cat.setBreed(catBreedEnum);
+            i++;
+            animalList.add(cat);
+        }
+        return animalList;
+    }
+
+    //--------------------------------------------------------------
+
+    public Predicate Q1Filter(){
+        Predicate p1 = animal -> ((Cat)animal).getBreed().toString().startsWith("A");
+        Predicate p2 = animal -> ((Cat)animal).getBreed().toString().startsWith("C");
+        Predicate p3 = animal -> ((Cat)animal).getBreed().toString().startsWith("P");
+        Predicate p4 = p1.or(p2).or(p3);
+        return p4;
+    }
+
+    //--------------------------------------------------------------
+
+    public Predicate Q2Filter(){
+        Predicate<Animal> p2 = animal -> {
+            int a = Integer.parseInt(animal.getName().substring(3));
+            return a%2 ==0;
+        };
+        return p2;
+    }
+
+}
--- Ex1_Java/src/main/java/MainClass.java
+++ Ex1_Java/src/main/java/MainClass.java
@@ -6,23 +6,17 @@ import java.util.stream.Stream;
 
 public class MainClass {
     public static void main(String[] args) {
-        List<Animal> animalList = new ArrayList<>();
-        int i = 1;
-        for (CatBreedEnum catBreedEnum : CatBreedEnum.values()) {
 
-            Cat cat = new Cat("cat"+i);
-            cat.setBreed(catBreedEnum);
-            i++;
-            animalList.add(cat);
-        }
+        AnimalManger animalManger = new AnimalManger();
 
+        List<Animal> animalList = animalManger.createCatList();
         Stream<Animal> stream = animalList.stream();
 
         // Q1.
-        Predicate p1 = Q1Filter();
+        Predicate p1 = animalManger.Q1Filter();
 
         // Q2.
-        Predicate p2 = Q2Filter();
+        Predicate p2 = animalManger.Q2Filter();
 
         List<CatBreedEnum> catBreedEnums = (List<CatBreedEnum>) stream
                 .filter(p1)
@@ -31,28 +25,5 @@ public class MainClass {
                 .collect(Collectors.toList());
 
         System.out.println(catBreedEnums);
-
     }
-
-    //--------------------------------------------------------------
-
-    static Predicate Q1Filter(){
-        Predicate p1 = animal -> ((Cat)animal).getBreed().toString().startsWith("A");
-        Predicate p2 = animal -> ((Cat)animal).getBreed().toString().startsWith("C");
-        Predicate p3 = animal -> ((Cat)animal).getBreed().toString().startsWith("P");
-        Predicate p4 = p1.or(p2).or(p3);
-        return p4;
-    }
-
-    //--------------------------------------------------------------
-
-    static Predicate Q2Filter(){
-        Predicate<Animal> p2 = animal -> {
-            int a = Integer.parseInt(animal.getName().substring(3));
-            return a%2 ==0;
-        };
-        return p2;
-    }
-
-    //--------------------------------------------------------------
 }
