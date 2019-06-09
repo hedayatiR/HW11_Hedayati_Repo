5a3a75e339c3bb89adecc3aebe245aa0b9ab62ad
hedayatiR <gunner_1369@yahoo.com>
2019-06-09 10:44:54 +0430

    Part1: Classses definition

---
+++ Ex1_Java/src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat extends Animal {
+    private String breed;
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+}
